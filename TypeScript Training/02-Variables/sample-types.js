f72ff4eeb6e741818bfc8f21a20792af9d6c67ad
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2024-11-26 10:00:09 -0300

    TypeScript Variables

---
+++ TypeScript Training/02-Variables/sample-types.js
@@ -0,0 +1,13 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Samuel";
+var lastName = "Iamarino";
+//Let's Break the code!
+// found = 0;
+// grade = "a";
+// firstName = false;
+console.log(found);
+console.log("Your grade is: " + grade);
+console.log("My name is " + firstName);
+console.log("Hello my name is ".concat(firstName, " ").concat(lastName));
+console.log("My last name is " + lastName);
---
+++ TypeScript Training/02-Variables/sample-types.ts
@@ -0,0 +1,15 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Samuel";
+let lastName: string = "Iamarino";
+
+//Let's Break the code!
+// found = 0;
+// grade = "a";
+// firstName = false;
+
+console.log(found);
+console.log("Your grade is: " + grade);
+console.log("My name is " + firstName);
+console.log(`Hello my name is ${firstName} ${lastName}`);
+console.log("My last name is " + lastName);
