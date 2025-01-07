dfa08205d36594fdd41576632ffcc3dff03a5a8d
Samuel Ferla Iamarino <125980333+SamIamarino@users.noreply.github.com>
2025-01-07 14:37:38 -0300

    Section 8 - Adding bootstrap

--- AngularTraining/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ AngularTraining/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { SalesPerson } from './sales-person';
- 
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sales-person-list',
-  imports: [],
-  templateUrl: './sales-person-list.component.html',
-  styleUrl: './sales-person-list.component.css'
+  imports: [CommonModule],
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrl: './sales-person-list.component.css',
 })
 export class SalesPersonListComponent {
-
   salesPersonList: SalesPerson[] = [
-    new SalesPerson('Anup','Kumar','anup.kumar@gmail.com', 4000 ),
-    new SalesPerson('Kleber','Freitas','kleber.freitas@gmail.com', 3000 ),
-    new SalesPerson('Jose','Anisio','jose.123@gmail.com', 2000 ),
-    new SalesPerson('Vilmar ','Cavalcante','vilmar.cav@gmail.com', 1000 ),
-    new SalesPerson('Milton','Nascimento','Milton.nascimento@gmail.com', 14000 )
+    new SalesPerson('Anup', 'Kumar', 'anup.kumar@gmail.com', 4000),
+    new SalesPerson('Kleber', 'Freitas', 'kleber.freitas@gmail.com', 3000),
+    new SalesPerson('Jose', 'Anisio', 'jose.123@gmail.com', 2000),
+    new SalesPerson('Vilmar ', 'Cavalcante', 'vilmar.cav@gmail.com', 1000),
+    new SalesPerson('Vilmar ', 'Cavalcante', 'vilmar.cav@gmail.com', 60000),
+    new SalesPerson(
+      'Milton',
+      'Nascimento',
+      'Milton.nascimento@gmail.com',
+      14000
+    ),
   ];
-
-
 }
